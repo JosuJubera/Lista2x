9b04ca9b8f6a2c2e4332d84285be0a210cd2d533
juber <you@example.com>
2015-05-06 16:37:09 +0200

    Añadido buscador

--- index.php
+++ index.php
@@ -6,29 +6,18 @@
 	$id = 1;
 
 	session_start();
-    if (isset($_SESSION['admin'])){
-        $accion="AR";
-        $id=1;
-    }
+	
     if(isset($_GET["accion"]))
     {
         $accion = $_GET["accion"];
-    }
-    if(isset($_POST["accion"]))
-    {
-        $accion = $_POST["accion"];
     }
 
     if(isset($_GET["id"]))
     {
          $id = $_GET["id"];
     }
-    if(isset($_POST["id"]))
-    {
-         $id = $_POST["id"];
-    }
     
-    if(isset($_POST["uid"],$_POST["pw"])) //preguntar si debe de ir con un action y un id
+    if(isset($_POST["uid"],$_POST["pw"]))
     {
         $uid = $_POST["uid"];
         $pw = $_POST["pw"];
@@ -71,92 +60,7 @@
 	{
 		vmostrarContacto();
 	}
-        ////////////////////////////////////////////////////////////////////////
-        if ($accion=="admin"){//login admin
-            switch ($id){
-                case 2:$nombre=adminlogin($_POST['aid'], $_POST['apw']);
-                    if ($nombre!==false){
-                        $_SESSION['admin']=$nombre;
-                        vmostrarAmenu();//aqui iria el menu principal
-                    }else{//error ususario incorrecto
-                        vloginadmin();
-                    }
-                    break;
-                default:vloginadmin();
-            }
-        }
-        if ($accion== "AR"){ //administrar reportes
-             if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){
-                 switch ($id){
-                    case 1: vmostrarAmenu(); //mostrar reportes
-                            break;
-                    case 2://borrar comentario
-                    default:vmostrarAmenu();
-                }
-             }else{
-                 echo "no eres admin";
-             }
-        }
-        if ($accion == "AC"){//alta cancion
-            if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){ //es un admin
-                switch ($id){
-                    case 1: valtaCancion(); //mostrar el formulario
-                            break;
-                    case 2:if (añadirCancion($_POST['titulo'], $_POST['autor'], $_POST['album'], $_POST['genero'], $_POST['año'], $_FILES["caratula"],$_FILES["cancion"])){
-                                //exito, añadido. mostrar mensaje y pa dejar pa añadir otra
-                                echo "cancion añadida con exito";
-                                vmostrarAmenu();
-                            }else{
-                                //fallo, enviar mensajede fallo y k vuelva a intentarlo
-                                echo "fallo";
-                                valtacancion();
-                            }
-                }
-            }else{
-                echo "necesitas ser administrador";
-            }
-        }
-        if ($accion == "BC"){//borrar cancion
-            if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){ //es un admin
-                switch ($id){
-                    case 1: vbuscarborrar(); //mostrar el buscador
-                            break;
-                    case 2://mostrar resultados
-                        switch ($_GET['buscar']){
-                            case 1:$canciones=mbuscartitulo($_GET['busqueda']);//titulo
-                                vbuscarborrar($_GET['busqueda'],$_GET['buscar']);
-                                vcancionesborrar($canciones);
-                                break;
-                            case 2:$autores=  mbuscarautor($_GET['busqueda']);
-                                vbuscarborrar($_GET['busqueda'],$_GET['buscar']);
-                                vautorborrar($autores);
-                                break;
-                            case 3:$albumnes=mbuscaralbum($_GET['busqueda']);//album(disco)
-                                vbuscarborrar($_GET['busqueda'],$_GET['buscar']);
-                                valbumborrar($albumnes);
-                        }
-                        break;
-                    case 3: if (mborrarCancion($_GET['idcancion'])){
-                                echo "eliminado con exito";
-                                vmostrarAmenu();
-                            }else{
-                                echo 'fallo al eliminar';
-                                 vbuscarborrar($_GET['busqueda'],$_GET['buscar']);
-                                }
-                        break;
-                    case 4:$cancionesaborrar=cancionesautor($_GET['autor']);//confirmar borrar autor
-                            $_SESSION['cancionesborrar']=$cancionesaborrar;
-                            vmostrarconfirmacion($cancionesaborrar);
-                    case 5:$cancionesaborrar=cancionesalbum($_GET['album']);//confirmar borrar disco
-                            $_SESSION['cancionesborrar']=$cancionesaborrar;
-                            vmostrarconfirmacion($cancionesaborrar);
-                    case 6://eliminar canciones
-                }
-            }else{
-                echo "necesitas ser administrador";
-            }
-        }
-	/////////////////////////////////////////////////////////////////////////
+	
     if($accion == "TL")
     {
         if (!isset($_SESSION["usuario"]))
@@ -167,6 +71,7 @@
         else
         {
             vmostrarUsuario($_SESSION["usuario"]);
+            vmostrarBuscardor();
             vmostrarRmenu();
         }
         $datos = mToplistas();
@@ -184,7 +89,8 @@
         else
         {
             vmostrarUsuario($_SESSION["usuario"]);
-            vmostrarRmenu();
+            vmostrarBuscardor();
+		    vmostrarRmenu();
         }
 		$datos = mTopcanciones();
 		vmostrarTopcanciones($datos);
@@ -194,6 +100,7 @@
 	if (($accion == "ML") and (isset($_SESSION["usuario"])))
 	{
 		vmostrarUsuario($_SESSION["usuario"]);
+		vmostrarBuscardor();
         vmostrarRmenu();
 		$datos = mMislistas($_SESSION["usuario"]);
 		vmostrarMislistas($datos);
@@ -203,9 +110,85 @@
 	if (($accion == "MF") and (isset($_SESSION["usuario"])))
 	{
 		vmostrarUsuario($_SESSION["usuario"]);
-                vmostrarRmenu();
+		vmostrarBuscardor();
+        vmostrarRmenu();
 		$datos = mMisfavoritos($_SESSION["usuario"]);
 		vmostrarMisfavoritos($datos);
 		vmostrarContactar();
 	}
+	
+	////////////////////////////////////////////////////////////////////////
+	if ($accion=="admin"){//login admin
+		switch ($id){
+			case 2:$nombre=adminlogin($_POST['aid'], $_POST['apw']);
+				if ($nombre!==false){
+					$_SESSION['admin']=$nombre;
+					vmostrarAmenu();//aqui iria el menu principal
+				}else{//error ususario incorrecto
+					vloginadmin();
+				}
+				break;
+			default:vloginadmin();
+		}
+	}
+	if ($accion== "AR"){ //administrar reportes
+		 if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){
+			 switch ($id){
+				case 1: vmostrarAmenu(); //mostrar reportes
+						break;
+				case 2://borrar comentario
+				default:vmostrarAmenu();
+			}
+		 }else{
+			 echo "no eres admin";
+		 }
+	}
+	if ($accion == "AC"){//alta cancion
+		if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){ //es un admin
+			switch ($id){
+				case 1: valtaCancion(); //mostrar el formulario
+						break;
+				case 2:if (añadirCancion($_POST['titulo'], $_POST['autor'], $_POST['album'], $_POST['genero'], $_POST['año'], $_FILES["caratula"],$_FILES["cancion"])){
+							//exito, añadido. mostrar mensaje y pa dejar pa añadir otra
+							echo "cancion añadida con exito";
+							vmostrarAmenu();
+						}else{
+							//fallo, enviar mensajede fallo y k vuelva a intentarlo
+							echo "fallo";
+							valtacancion();
+						}
+			}
+		}else{
+			echo "necesitas ser administrador";
+		}
+	}
+	if ($accion == "BC"){//borrar cancion
+		if (isset($_SESSION["admin"]) /*&& mIsAdmin($_SESSION["admin"])*/){ //es un admin
+			switch ($id){
+				case 1: vbuscarborrar(); //mostrar el buscador
+						break;
+				case 2://mostrar resultados
+					switch ($_GET['buscar']){
+						case 1:$canciones=mbuscartitulo($_GET['busqueda']);//titulo
+							vbuscarborrar($_GET['busqueda'],$_GET['buscar']);
+							vcancionesborrar($canciones);
+							break;
+						case 2://autor
+						case 3://disco
+					}
+					break;
+				case 3: if (mborrarCancion($_GET['idcancion']))
+							echo "eliminado con exito";
+						else
+							echo 'fallo al eliminar';
+					break;
+				case 4://eliminar disco
+				case 5://eliminar album
+				case 6://comfirmar
+			}
+		}else{
+			echo "necesitas ser administrador";
+		}
+	}
+	/////////////////////////////////////////////////////////////////////////
 ?>
--- vista.php
+++ vista.php
@@ -31,18 +31,24 @@
 		$aux = file_get_contents("fonts/alta.html");
 		echo $aux;
 	}
+	
+	function vmostrarBuscardor()
+	{
+		$aux = file_get_contents("fonts/buscar.html");
+		echo $aux;
+	}
     
     function vmostrarImenu()
     {
         $aux = leerfichero("fonts/menu.html");
-        $partes = explode("##INVITADO##", $aux);
-        echo $partes[0] . $partes[1];
+        $partes = explode("##REGISTRADO##", $aux);
+        echo $partes[0] . $partes[2];
     }
 
     function vmostrarRmenu()
     {
         $aux = leerfichero("fonts/menu.html");
-        $partes = explode("##INVITADO##", $aux);
+        $partes = explode("##REGISTRADO##", $aux);
         echo $partes[0] . $partes[1] . $partes[2];
     }
 
@@ -57,12 +63,11 @@
 		{
 			$lista = $partes[1];
 			$lista = str_replace("##POSICION##", $i, $lista);
-			$lista = str_replace("##ID##", $datos[0], $lista);
 			$lista = str_replace("##NOMBRE##", $datos[2], $lista);
 			$lista = str_replace("##ASUNTO##", $datos[3], $lista);
 			$lista = str_replace("##N##", $datos[4], $lista);
 			$lista = str_replace("##AUTOR##", $datos[1], $lista);
-			$lista = str_replace("##VALORACION##", $datos[5], $lista);
+			$lista = str_replace("##VALORACION##", $datos[7], $lista);
 			$contenido .= $lista;
 			$i++;
 		}
@@ -98,16 +103,16 @@
 		$contenido = "";
 		$lista = "";
 		$i = 1;
-		while ($datos = mysql_fetch_assoc($consulta))
+		while ($datos = mysql_fetch_array($consulta))
 		{
 			$lista = $partes[1];
 			$lista = str_replace("##POSICION##", $i, $lista);
-			$lista = str_replace("##ID##", $datos['Id'], $lista);
-			$lista = str_replace("##NOMBRE##", $datos['Titulo'], $lista);
-			$lista = str_replace("##ASUNTO##", $datos['Asunto'], $lista);
-			$lista = str_replace("##N##", $datos[''], $lista); /* @TODO @TO DO revisar la consulta*/
-			$lista = str_replace("##AUTOR##", $datos['Usuario'], $lista);
-			$lista = str_replace("##VALORACION##", $datos['ValoracionSemanal'], $lista);
+			$lista = str_replace("##ID##", $datos[0], $lista);
+			$lista = str_replace("##NOMBRE##", $datos[2], $lista);
+			$lista = str_replace("##ASUNTO##", $datos[3], $lista);
+			$lista = str_replace("##N##", $datos[6], $lista);
+			$lista = str_replace("##AUTOR##", $datos[1], $lista);
+			$lista = str_replace("##VALORACION##", $datos[7], $lista);
 			$contenido .= $lista;
 			$i++;
 		}
@@ -146,119 +151,57 @@
 		$aux = file_get_contents("fonts/contacto.html");
 		echo $aux;
 	}
-        function valtacancion(){
+	
+	function valtacancion(){
             $aux=  file_get_contents('admin/altacancion.html');
             echo $aux;
         }
-        /////////////////////////////////////////////////////////////
-        function vloginadmin(){
-            $aux=  file_get_contents('admin/adminlogin.html');
-            echo $aux;
-        }
-        function vmostrarAmenu(){
-            $aux=  file_get_contents('admin/menu.html');
-            echo $aux;
-        }
-        function vbuscarborrar($buscar="",$tipo=1){
-            $aux=file_get_contents('admin/buscar.html');
-            $aux =str_replace("##BUSCAR##", $buscar, $aux);
-            switch ($tipo){ //1 titulo, 2 autor 3 disco
-                case 1:$aux=str_replace("##TCHK##", 'checked', $aux);
-                    break;
-                case 2:$aux=str_replace("##ACHK##", 'checked', $aux);
-                    break;
-                case 3:$aux=str_replace("##DCHK##", 'checked', $aux);
-                    break;
-            }
-            //limpiamos marcas
-            $aux=str_replace("##TCHK##", '', $aux);
-            $aux=str_replace("##ACHK##", '', $aux);
-            $aux=str_replace("##DCHK##", '', $aux); 
-            echo $aux;
-        }
-        function vcancionesborrar($canciones){
-            $aux = leerfichero("admin/canciones.html");
-            $partes = explode("##FILALISTA##", $aux);
-            $contenido = "";
-            $lista = "";
-            if ($canciones!=null){
-                foreach ($canciones as $cancion) {
-                    $lista = $partes[1];
-                    $lista = str_replace("##TITULO##", $cancion['Titulo'], $lista);
-                    $lista = str_replace("##AUTOR##", $cancion['Autor'], $lista);
-                    $lista = str_replace("##ALBUM##", $cancion['Album'], $lista);
-                    $lista = str_replace("##GENERO##",$cancion['Genero'], $lista);
-                    $lista = str_replace("##AÑO##", $cancion['Año'], $lista);
-                    $lista = str_replace("##IDCANCION##", $cancion['Id'], $lista);
-                    $contenido .= $lista;
-                }
+	/////////////////////////////////////////////////////////////
+	function vloginadmin(){
+		$aux=  file_get_contents('admin/adminlogin.html');
+		echo $aux;
+	}
+	function vmostrarAmenu(){
+		$aux=  file_get_contents('admin/menu.html');
+		echo $aux;
+	}
+	function vbuscarborrar($buscar="",$tipo=1){
+		$aux=file_get_contents('admin/buscar.html');
+		$aux =str_replace("##BUSCAR##", $buscar, $aux);
+		switch ($tipo){ //1 titulo, 2 autor 3 disco
+			case 1:$aux=str_replace("##TCHK##", 'checked', $aux);
+				break;
+			case 2:$aux=str_replace("##ACHK##", 'checked', $aux);
+				break;
+			case 3:$aux=str_replace("##DCHK##", 'checked', $aux);
+				break;
+		}
+		//limpiamos marcas
+		$aux=str_replace("##TCHK##", '', $aux);
+		$aux=str_replace("##ACHK##", '', $aux);
+		$aux=str_replace("##DCHK##", '', $aux); 
+		echo $aux;
+	}
+	function vcancionesborrar($canciones){
+		$aux = leerfichero("admin/canciones.html");
+		$partes = explode("##FILALISTA##", $aux);
+		$contenido = "";
+		$lista = "";
+		if ($canciones!=null){
+			foreach ($canciones as $cancion) {
+				$lista = $partes[1];
+				$lista = str_replace("##TITULO##", $cancion['Titulo'], $lista);
+				$lista = str_replace("##AUTOR##", $cancion['Autor'], $lista);
+				$lista = str_replace("##ALBUM##", $cancion['Album'], $lista);
+				$lista = str_replace("##GENERO##",$cancion['Genero'], $lista);
+				$lista = str_replace("##AÑO##", $cancion['Año'], $lista);
+				$lista = str_replace("##IDCANCION##", $cancion['Id'], $lista);
+				$contenido .= $lista;
+			}
 
-                echo $partes[0] . $contenido . $partes[2];
-            }else{
-                echo "<p> No se han encontrado canciones con ese criterio</p>";
-            }
-        }
-        function vautorborrar($autores){
-            $aux = leerfichero("admin/autor.html");
-            $partes = explode("##FILALISTA##", $aux);
-            $contenido = "";
-            $lista = "";
-            if ($autores!=null){
-                foreach ($autores as $autor) {
-                    $lista = $partes[1];
-                    $lista = str_replace("##AUTOR##", $autor['autor'], $lista);
-                    $lista = str_replace("##NALBUM##",$autor['albumnes'], $lista);
-                    $lista = str_replace("##NCANCION##", $autor['canciones'], $lista);
-                    $contenido .= $lista;
-                }
-
-                echo $partes[0] . $contenido . $partes[2];
-            }else{
-                echo "<p> No se han encontrado artistas con ese criterio</p>";
-            }
-        }
-        
-        function valbumborrar($albumnes){
-            $aux = leerfichero("admin/disco.html");
-            $partes = explode("##FILALISTA##", $aux);
-            $contenido = "";
-            $lista = "";
-            if ($albumnes!=null){
-                foreach ($albumnes as $album) {
-                    $lista = $partes[1];
-                    $lista = str_replace("##AUTOR##", $album['autor'], $lista);
-                    $lista = str_replace("##ALBUM##",$album['album'], $lista);
-                    $lista = str_replace("##NCANCION##", $album['canciones'], $lista);
-                    $contenido .= $lista;
-                }
-
-                echo $partes[0] . $contenido . $partes[2];
-            }else{
-                echo "<p> No se han encontrado albumnes con ese criterio</p>";
-            } 
-        }
-        function vmostrarconfirmacion($canciones){
-            echo "<p>Esta seguro de que desea eliminar las siguientes canciones?</p>";
-            $aux = leerfichero("admin/canciones.html");
-            $partes = explode("##FILALISTA##", $aux);
-            $contenido = "";
-            $lista = "";
-            if ($canciones!=null){
-                foreach ($canciones as $cancion) {
-                    $lista = $partes[1];
-                    $lista = str_replace("##TITULO##", $cancion['Titulo'], $lista);
-                    $lista = str_replace("##AUTOR##", $cancion['Autor'], $lista);
-                    $lista = str_replace("##ALBUM##", $cancion['Album'], $lista);
-                    $lista = str_replace("##GENERO##",$cancion['Genero'], $lista);
-                    $lista = str_replace("##AÑO##", $cancion['Año'], $lista);
-                    $lista = str_replace("##IDCANCION##", $cancion['Id'], $lista);
-                    $contenido .= $lista;
-                }
-
-                echo $partes[0] . $contenido . $partes[2];
-            }else{
-                echo "<p> No se han seleccionado canciones para borrar</p>";
-            }
-            echo "<a href='index.php?accion=BC&id=6'>Confirmar</a>";
-        }
+			echo $partes[0] . $contenido . $partes[2];
+		}else{
+			echo "<p> No se han encontrado canciones con ese criterio</p>";
+		}
+	}
 ?>
