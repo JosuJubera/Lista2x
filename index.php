96d0bd7f1b75507546cd083407941ff6e2c01e50
juber <you@example.com>
2015-05-23 16:29:52 +0200

    Contactar modificau, no definitivo

--- index.php
+++ index.php
@@ -126,10 +126,43 @@
 	}
 	
         //mostrar contacto?????
+	/*
 	if ($accion == "C")
 	{
-		vmostrarContacto();
-	}
+		switch($id)
+		{
+			case 1: 	vmostrarContacto();
+						break;
+			case 2: 	@$nombre = addslashes($_POST['uid']);
+						@$email = addslashes($_POST['cu']);
+						@$mensaje = addslashes($_POST['message']);
+						 
+						//Preparamos el mensaje de contacto
+						$cabeceras = "From: $email\n" //La persona que envia el correo
+						 . "Reply-To: $email\n";
+						$asunto = "Contacto Lista2x"; //asunto aparecera en la bandeja del servidor de correo
+						$email_to = "josujubera@hotmail.com"; //cambiar por tu email
+						$contenido = "$nombre ha enviado un mensaje desde la web de Lista2x\n"
+						. "\n"
+						. "Nombre: $nombre\n"
+						. "Email: $email\n"
+						. "Mensaje: $mensaje\n"
+						. "\n";
+						 
+						//Enviamos el mensaje y comprobamos el resultado
+						if (@mail($email_to, $asunto ,$contenido ,$cabeceras ))
+						{
+							//Si el mensaje se envía muestra una confirmación
+							die("Gracias, su mensaje se envio correctamente.");
+						}
+						else
+						{
+							//Si el mensaje no se envía muestra el mensaje de error
+							die("Error: Su información no pudo ser enviada, intente más tarde");
+						}
+						break;
+		}	
+	}*/
 	
         //buscar
 	if (($accion == "B") and (isset($_GET["search"])))
